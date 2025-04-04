c235a7fc33dc6ca4ca35a09ac43bdd6e3846b37f
Mateus Tosta <116604644+mateustostadev@users.noreply.github.com>
2025-04-04 16:05:19 -0300

    fix: atualização da imagem do projeto cantinho das flores

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -28,7 +28,7 @@ const projects = [
     description:
       "Sistema E-commerce para uma floricultura localizada em Salvador-BA, especializados na venda de flores e arranjos personalizados para diversas ocasiões.",
     image:
-      "https://images.unsplash.com/photo-1508253578933-2905be28d772?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      "https://plus.unsplash.com/premium_photo-1676475964992-6404b8db0b53?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     tech: ["Wordpress", "PHP", "MySQL", "Mercado Pago API"],
     link: "https://cantinhodasflores.com",
   },
